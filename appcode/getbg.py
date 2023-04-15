eab72d5e210c0786efcd59bf732b4b5a52c8ca08
KSBolton <ksbolton@gmail.com>
2023-04-15 10:21:58 +0000

    err handling

--- appcode/getbg.py
+++ appcode/getbg.py
@@ -4,9 +4,10 @@ import boto3
 BGIMAGE = os.environ.get("BGIMAGE") 
 
 s3 = boto3.client("s3")
-split_str = BGIMAGE.split('/')
+
 try:
+    split_str = BGIMAGE.split('/')
     s3.download_file(Bucket=split_str[2],
                     Key=split_str[-1], Filename="/app/static/img/bgimg.jpg")
-except NoCredentialsError as e:
+except (AttributeError, NoCredentialsError) as e:
     print(e)                    
