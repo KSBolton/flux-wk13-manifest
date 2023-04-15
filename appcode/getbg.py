54d3f2b39be837e4fcb5072240325d2d390d960c
KSBolton <ksbolton@gmail.com>
2023-04-15 20:34:16 +0000

    change

--- appcode/getbg.py
+++ appcode/getbg.py
@@ -5,6 +5,7 @@ from botocore.exceptions import NoCredentialsError
 BGIMAGE = open("/etc/webappinfo/bgimage")
 
 s3 = boto3.client("s3")
+split_str = ""
 
 try:
     split_str = BGIMAGE.split('/')
@@ -14,5 +15,5 @@ except (AttributeError) as e:
 try:
     s3.download_file(Bucket=split_str[2],
                     Key=split_str[-1], Filename="/app/static/img/bgimg.jpg")
-except (NoCredentialsError) as e:
+except as e:
     print(e)                    
